ba057a2e0be9f00d33d266c7a8aa212b25a8f8f8
Mosirezaei <132272882+Mosirezaei@users.noreply.github.com>
2026-09-04 08:20:18 +0400

    feat(blog): add route simcard-internet-apps-yerevan

---
+++ app/blog/simcard-internet-apps-yerevan/page.jsx
@@ -0,0 +1,10 @@
+import SimcardInternetYerevan from '@/views/blog/SimcardInternetYerevan';
+
+export const metadata = {
+  title: 'سیم‌کارت، اینترنت و اپلیکیشن‌های ضروری ایروان | گروه کاسپین',
+  alternates: { canonical: 'https://caspian.am/blog/simcard-internet-apps-yerevan' },
+};
+
+export default function Page() {
+  return <SimcardInternetYerevan />;
+}
